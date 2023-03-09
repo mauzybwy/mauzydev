2d46c1be3f1add8367a4f9b818ab3ae286e5e6d2
Brandon <mauzybwy@gmail.com>
2023-03-09 18:07:46 -0500

    Ditch the scrolling thing

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Import
  *****************************************************************************/
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 
 import { useWindowDimensions } from "hooks/window";
@@ -14,52 +14,54 @@ import colors from "style/colors";
  *****************************************************************************/
 export default function Home () {
   const isMobile = useMobileCheck();
-  const boxRef = useRef<HTMLElement>(null);
-  const aboutRef = useRef<HTMLDivElement>(null);
-  const portfolioRef = useRef<HTMLDivElement>(null);
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [state, setState] = useState<"init"|"about"|"portfolio"|"connect">("init");
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOpacity(1);
+    }, 250)
+  }, [state])
 
   const { width, height } = useWindowDimensions();
   
   const handleScroll = (e) => {
     const target = e.target as HTMLTextAreaElement;
     console.log('Current scroll position:', target.scrollTop);
-    setScrollPosition(target.scrollTop);
   };
 
-  /* 
-   * 
-   * let boxWidth = 400 + (scrollPosition > 340 ? 340 : scrollPosition);
-   * let boxHeight = 170 + (
-   *   scrollPosition > 680 ? (
-   *     170 + (680 - 360)/2
-   *   ) : scrollPosition > 360 ? (
-   *     170 + (scrollPosition - 360) / 2
-   *   ) : (
-   *     scrollPosition / 2
-   * )); */
+  const _boxWidth = {
+    init: 250,
+    about: 610,
+    portfolio: 1200,
+    connect: 610,
+  }[state];
+  const boxWidth = _boxWidth > width - 64 ? width - 64 : _boxWidth;
 
-  let boxWidth = (250 + scrollPosition) > width - 64 ? width - 64 : 250 + scrollPosition;
-  let boxHeight = 170 + (
-    scrollPosition > 680 ? (
-      170 + (680 - 360)/2
-    ) : scrollPosition > 360 ? (
-      170 + (scrollPosition - 360) / 2
-    ) : (
-      scrollPosition / 2
-  ));
+  const _boxHeight = {
+    init: 170,
+    about: 340,
+    portfolio: 600,
+    connect: 600,
+  }[state];
+  const boxHeight = _boxHeight > height - 64 ? height - 64 : _boxHeight;
 
-  const handleClickName = () => {
-    boxRef.current.scroll({ top: 0, behavior: "smooth" });
-  }
+  const body = {
+    init: <Init />,
+    about: <About />,
+    portfolio: <Portfolio />,
+    connect: <Connect />,
+  }[state];
 
-  const handleClickAbout = () => {
-    //aboutRef.current.scrollIntoView({ behavior: "smooth" });
-    boxRef.current.scroll({ top: 340, behavior: "smooth" });
-  }
-
-  const handleClickPortfolio = () => {
-    boxRef.current.scroll({ top: 1006, behavior: "smooth" });
+  const updateState = (tab) => {
+    setState(tab)
+    /* if (tab !== state) {
+     *   setOpacity(0)
+     *   setTimeout(() => {
+     *     setState(tab)
+     *   }, 200)
+     * } */
   }
   
   return (
@@ -70,110 +72,123 @@ export default function Home () {
         <Box display="flex" flexDirection="column">
           <Box display="flex" flexDirection="column" style={{ gap: "16px" }}>
             <Box display="flex" justifyContent="space-between">
-              <Typography variant="h5" className="interact disable-select" onClick={handleClickName}>
+              <Typography variant="h5" className="interact disable-select" onClick={() => updateState("init")}>
                 brandon wetzel
               </Typography>
             </Box>
             <Box display="flex" style={{ gap: "16px" }}>
-              <Typography className="interact disable-select" onClick={handleClickAbout}>
-                about
-              </Typography>
-              <Typography className="interact disable-select" onClick={handleClickPortfolio}>
-                portfolio
-              </Typography>
-              <Typography className="interact disable-select" onClick={handleClickPortfolio}>
-                connect
-              </Typography>
+              {["about", "portfolio", "connect"].map(tab => (
+                <Typography
+                  key={tab}
+                  className="interact disable-select"
+                  onClick={() => updateState(tab)}
+                  style={{ textDecoration: state === tab ? "underline" : undefined }}
+                >
+                  {tab}
+                </Typography>
+              ))}
             </Box>
           </Box>
           <Box
-            //p="16px"
             width={boxWidth}
             height={boxHeight}
             style={{
               border: `2px solid ${colors.accent}`,
-              //backgroundColor: colors.mid,
-              backgroundClip: "content-box"
+              backgroundClip: "content-box",
+              transition: "all 0.75s",
             }}
           >
             <Box
-              ref={boxRef}
-              onScroll={handleScroll}
+              p="16px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              width="100%"
+              height="100%"
               className="hide-scrollbar"
-              style={{ width:"100%", height:"100%", overflow:"hidden", scrollSnapType: "y mandatory" }}
+              style={{
+                overflow: "scroll",
+                opacity: opacity,
+                transition: "all 0.25s",
+                boxSizing: "border-box",
+              }}
             >
-              <Box display="flex" justifyContent="center" alignItems="center" height="100%" width="100%">
-                <Typography className="disable-select">hello there :)</Typography>
-              </Box>
-              <Box
-                className="hide-scrollbar"
-                ref={aboutRef}
-                height="100%"
-                width="100%"
-                p="32px"
-                boxSizing="border-box"
-                style={{ overflow: "scroll" }}
-              >
-                <Typography>
-                  Hi, i'm BRANDON and i write software. I have a BS in COMPUTER ENGINEERING from UNIVERSITY OF MARYLAND and
-                  spent 5 years working on embedded systems at TEXAS INSTRUMENTS.
-                  I have since moved on to making WEB and MOBILE applications.
-                </Typography>
-              </Box>
-              <Box
-                className="hide-scrollbar"
-                ref={portfolioRef}
-                height="100%"
-                width="100%"
-                p="32px"
-                boxSizing="border-box"
-                style={{ overflow: "scroll" }}
-              >
-                <Typography>
-                  asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
-                  al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
-                  alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
-                  flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
-
-                  asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
-                  al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
-                  alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
-                  flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
-
-                  asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
-                  al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
-                  alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
-                  flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
-
-                  asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
-                  al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
-                  alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
-                  flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
-
-                  asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
-                  al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
-                  alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
-                  flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
-
-                  asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
-                  al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
-                  alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
-                  flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
-                </Typography>
-              </Box>
-              <Box height="1000px" />
+              {body}
             </Box>
           </Box>
-          {/* <Box display="flex" style={{ gap: "16px" }}>
-              <Typography className="interact disable-select" onClick={handleClickAbout}>
-              about
-              </Typography>
-              <Typography className="interact disable-select" onClick={handleClickPortfolio}>
-              portfolio
-              </Typography>
-              </Box> */}
         </Box>
       </Box>
     </PageContainer>
+);
+}
+
+/*****************************************************************************
+ * Helper Components
+ *****************************************************************************/
+
+const Init = () => {
+  return (
+    <Typography>
+      hello there :)
+    </Typography>
+  );
+}
+
+const About = () => {
+  return (
+    <Typography style={{ alignSelf: "start" }}>
+      Hi, i'm BRANDON and i write software. I have a BS in COMPUTER ENGINEERING from UNIVERSITY OF MARYLAND and
+      spent 5 years working on embedded systems at TEXAS INSTRUMENTS.
+      I have since moved on to making WEB and MOBILE applications.
+    </Typography>
+  );
+}
+
+const Portfolio = () => {
+  return (
+    <Typography style={{ alignSelf: "start", overflow: "scroll" }}>
+      asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
+      al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
+      alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
+      flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
+      <br />
+      <br />
+      asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
+      al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
+      alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
+      flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
+      <br />
+      <br />
+      asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
+      al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
+      alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
+      flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
+      <br />
+      <br />
+      asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
+      al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
+      alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
+      flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
+      <br />
+      <br />
+      asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
+      al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
+      alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
+      flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
+      <br />
+      <br />
+      asldfjla jdflkja lskdflkasjdflk alksdjf lkasdflj al kdfj lkajdf lkajdflk asjdf
+      al skdfjlaksdfj lkasjdf lkajs dflkjasldkfjalskdfjalskdjflkasjd flkajs dlfkjaskdf
+      alskdfj laksjd flkasj dflk jalksdfjlkajsdlkfjalksd jflkasjd
+      flaks dflkajsd flkja sldkfj alksdfj lkasjd flkajsdf
+    </Typography>
+  );
+}
+
+const Connect = () => {
+  return (
+    <Typography style={{ alignSelf: "start" }}>
+      hello there :)
+    </Typography>
   );
 }
