4104e6dbf82264478feb12d4e92d116b13aab7ef
Brandon <mauzybwy@gmail.com>
2023-03-10 09:21:50 -0500

    More consistent scrolling

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Import
  *****************************************************************************/
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Box, Typography } from "@mui/material";
 
 import { useWindowDimensions } from "hooks/window";
@@ -49,19 +49,53 @@ export default function Home () {
     connect: <Connect />,
   }[state];
 
+  const initRef = useRef(null);
+  const aboutRef = useRef(null);
+  const portfolioRef = useRef(null);
+  const connectRef = useRef(null);
+
+  const blah = [
+    {
+      body: <Init />,
+      ref: initRef,
+    },
+    {
+      body: <About />,
+      ref: aboutRef,
+    },
+    {
+      body: <Portfolio />,
+      ref: portfolioRef,
+    },
+    {
+      body: <Connect />,
+      ref: connectRef,
+    },
+  ];
+
   useEffect(() => {
     setTimeout(() => {
       setBody(_body)
       setOpacity(1)
     }, 250);
   }, [state])
-  
 
   const updateState = (tab) => {
     setState(tab)
     if (tab !== state) {
       setOpacity(0)
     }
+
+    const onClick = {
+      init: () => initRef.current.scrollIntoView({ behavior: "smooth" }),
+      about: () => aboutRef.current.scrollIntoView({ behavior: "smooth" }),
+      portfolio: () => portfolioRef.current.scrollIntoView({ behavior: "smooth" }),
+      connect: () => connectRef.current.scrollIntoView({ behavior: "smooth" }),
+    }[tab];
+
+    setIntervalX(() => {
+      onClick()
+    }, 10, 75)    
   }
   
   return (
@@ -95,26 +129,16 @@ export default function Home () {
             style={{
               border: `2px solid ${colors.accent}`,
               backgroundClip: "content-box",
-              transition: "all 0.75s",
+              transition: "height 0.3s, width 0.6s",
+              //transition: "all 0.5 ease-in",
+              overflow: "hidden",
             }}
           >
-            <Box
-              p="16px"
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              width="100%"
-              height="100%"
-              className="hide-scrollbar"
-              style={{
-                overflow: "scroll",
-                opacity: opacity,
-                transition: "all 0.25s",
-                boxSizing: "border-box",
-              }}
-            >
-              {body}
-            </Box>
+            {blah.map(item => (
+              <Container innerRef={item.ref}>
+                {item.body}
+              </Container>
+            ))}
           </Box>
         </Box>
       </Box>
@@ -147,7 +171,17 @@ const About = () => {
 
 const Portfolio = () => {
   return (
-    <Box />
+    <Typography style={{ alignSelf: "start" }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Volutpat blandit aliquam etiam erat velit scelerisque. Sodales ut eu sem integer vitae justo eget magna fermentum. Aliquet bibendum enim facilisis gravida neque. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Fringilla ut morbi tincidunt augue interdum velit. Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Enim nec dui nunc mattis enim ut. Proin libero nunc consequat interdum varius. Sodales neque sodales ut etiam sit. Nec ultrices dui sapien eget mi proin sed libero enim. Viverra orci sagittis eu volutpat odio facilisis mauris sit amet.
+
+Tincidunt vitae semper quis lectus nulla at. Dolor purus non enim praesent elementum. Bibendum est ultricies integer quis auctor elit sed vulputate mi. Iaculis nunc sed augue lacus viverra vitae congue eu consequat. Sit amet mauris commodo quis. At urna condimentum mattis pellentesque id nibh. Volutpat diam ut venenatis tellus in metus. Amet purus gravida quis blandit turpis. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Blandit aliquam etiam erat velit scelerisque in dictum non. Enim ut sem viverra aliquet eget.
+
+Massa sed elementum tempus egestas sed sed. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Elit scelerisque mauris pellentesque pulvinar pellentesque. Aenean pharetra magna ac placerat vestibulum. Leo duis ut diam quam nulla porttitor massa. Integer vitae justo eget magna fermentum iaculis eu non. Vulputate ut pharetra sit amet. Convallis aenean et tortor at risus viverra. Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. Massa ultricies mi quis hendrerit dolor magna eget. Enim diam vulputate ut pharetra sit amet. Etiam erat velit scelerisque in. Egestas sed sed risus pretium quam. Mi quis hendrerit dolor magna eget est lorem. Leo in vitae turpis massa. Nec ullamcorper sit amet risus nullam.
+
+Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Quisque non tellus orci ac auctor augue. Non nisi est sit amet. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Aliquam malesuada bibendum arcu vitae elementum curabitur. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Volutpat odio facilisis mauris sit. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Faucibus in ornare quam viverra orci. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Morbi tincidunt ornare massa eget egestas purus viverra. Pellentesque elit eget gravida cum sociis natoque penatibus et. Enim nunc faucibus a pellentesque sit amet porttitor eget dolor.
+
+Diam quis enim lobortis scelerisque. In pellentesque massa placerat duis ultricies. Dictum at tempor commodo ullamcorper. A pellentesque sit amet porttitor eget dolor morbi. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Pharetra diam sit amet nisl suscipit. Velit egestas dui id ornare. In hac habitasse platea dictumst vestibulum rhoncus. Odio ut sem nulla pharetra diam. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Eget lorem dolor sed viverra ipsum nunc aliquet. Massa eget egestas purus viverra accumsan in nisl nisi. Amet mauris commodo quis imperdiet massa tincidunt. Diam phasellus vestibulum lorem sed risus ultricies. Ipsum dolor sit amet consectetur adipiscing. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada.
+    </Typography>
   );
 }
 
@@ -157,4 +191,43 @@ const Connect = () => {
       hello there :)
     </Typography>
   );
+}
+
+const Container = ({ children, innerRef }) => {
+  return (
+    <Box
+      ref={innerRef}
+      p="16px"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
+      height="100%"
+      className="hide-scrollbar"
+      style={{
+        overflow: "scroll",
+        //opacity: opacity,
+        transition: "all 0.25s",
+        boxSizing: "border-box",
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+/*****************************************************************************
+ * Helper Functions
+ *****************************************************************************/
+
+function setIntervalX(callback, delay, repetitions) {
+  var x = 0;
+  var intervalID = window.setInterval(function () {
+
+    callback();
+
+    if (++x === repetitions) {
+      window.clearInterval(intervalID);
+    }
+  }, delay);
 }
